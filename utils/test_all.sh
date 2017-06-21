48e45ded52d59484d4359be0e14a8539fde2a4ba
Daniel Kukharau <daniel.kukharau@gmail.com>
2017-06-21 17:40:23 +0300

    Migrate tests to Azure CLI 2.0.8

--- utils/test_all.sh
+++ utils/test_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script requires Azure CLI at least 2.0.8
 # Test creates Vms using templates.
 # All is done in resource group, which is created at begining and removed at the end.
 # Check steps are next:
@@ -25,7 +26,8 @@ DEPLOYMENT_NAME="${PREFIX}deployment$(date +%Y%m%d%H%M%S)"
 
 PARAMETERS_DIR="utils/params"
 
-if azure group show ${RESOURCE_GROUP} &> /dev/null; then
+RESPONSE=$(az group show --name ${RESOURCE_GROUP})
+if [ ${RESPONSE} ] ; then
     echo "Resource group ${RESOURCE_GROUP} exists.
 To avoid overlapping with other developers please change PREFIX in test script.
 Also you should modify parameter files accordingly.
@@ -34,40 +36,40 @@ find . -type f -exec sed -i 's/former_prefix/new_prefix/g' {} +"
     exit 1
 fi
 
-azure group create "${RESOURCE_GROUP}" "${LOCATION}"
+az group create --name "${RESOURCE_GROUP}" --location "${LOCATION}"
 
 echo "-------------------"
 echo "Test creation of all new elements"
-azure group deployment create \
-      -f "${AZURE_TEMPLATE_PATH}" \
-      --parameters-file "${PARAMETERS_DIR}/parameters.allnew.json" \
+az group deployment create \
+      --template-file "${AZURE_TEMPLATE_PATH}" \
+      --parameters @"${PARAMETERS_DIR}/parameters.allnew.json" \
       --resource-group "${RESOURCE_GROUP}" \
-      "${DEPLOYMENT_NAME}"
+      --name "${DEPLOYMENT_NAME}"
 
 echo "-------------------"
 echo "Test adding one more VM to existing elements"
 # Add Public Ip, will be used as existing.
-azure network public-ip create \
+az network public-ip create \
       --name "${PREFIX}pipexisting" \
       --allocation-method Dynamic \
-      --domain-name-label "${PREFIX}pipexisting" \
+      --dns-name "${PREFIX}pipexisting" \
       --resource-group "${RESOURCE_GROUP}" \
       --location "${LOCATION}"
 
 # Add VM
-azure group deployment create \
-      -f "${AZURE_TEMPLATE_PATH}" \
-      --parameters-file "${PARAMETERS_DIR}/parameters.existing.json" \
+az group deployment create \
+      --template-file "${AZURE_TEMPLATE_PATH}" \
+      --parameters @"${PARAMETERS_DIR}/parameters.existing.json" \
       --resource-group "${RESOURCE_GROUP}" \
-      "${DEPLOYMENT_NAME}"
+      --name "${DEPLOYMENT_NAME}"
 
 echo "-------------------"
 echo "Test adding one more VM to existing elements with no Public Ip and no availiability set"
-azure group deployment create \
-      -f "${AZURE_TEMPLATE_PATH}" \
-      --parameters-file "${PARAMETERS_DIR}/parameters.none.json" \
+az group deployment create \
+      --template-file "${AZURE_TEMPLATE_PATH}" \
+      --parameters @"${PARAMETERS_DIR}/parameters.none.json" \
       --resource-group "${RESOURCE_GROUP}" \
-      "${DEPLOYMENT_NAME}"
+      --name "${DEPLOYMENT_NAME}"
 
 # Remove resource group
-azure group delete --quiet "${RESOURCE_GROUP}"
+az group delete --name "${RESOURCE_GROUP}" --yes
